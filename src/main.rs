36f62dc7f809136335e2698d8c0781c7a24ec72c
wafrelka <wafrelka@gmail.com>
2023-09-03 03:47:57 +0900

    Simplify `--attribute` option parsing

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 struct Options {
-    #[clap(short, long)]
-    attribute: Option<Option<String>>,
+    #[clap(short, long, default_value = "data-ayarify")]
+    attribute: String,
 }
 
 fn main() {
-    let options = Options::parse();
-    let attr = options.attribute.map(|a| a.unwrap_or("data-ayarify".into()));
+    let Options { attribute } = Options::parse();
+    let attr = if attribute.is_empty() { None } else { Some(attribute) };
 
     let stdin = io::stdin();
     ayarify(&mut stdin.lock(), &mut io::stdout(), attr.as_deref()).expect("io error");
